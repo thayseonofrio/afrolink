bd7582fb7fd36f2ee126a3d12b6cb7f43a0a4c83
Thayse Onofrio <thayseonofrio@hotmail.com>
2020-06-14 19:34:14 -0300

    chore: configure development server

--- server/router/profile.js
+++ server/router/profile.js
@@ -3,7 +3,7 @@ import express from "express"
 import { createProfile } from "../controller/profile"
 
 const router = express.Router()
-console.log("router")
+
 router.post("/profile", createProfile)
 
 export default router;

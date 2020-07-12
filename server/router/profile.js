3a06d5bd9cbeb6ea55c8e65f0fc80136490c5ff4
Thayse Onofrio <thayseonofrio@hotmail.com>
2020-07-12 19:28:36 -0300

    feat: create profile in server

--- server/router/profile.js
+++ server/router/profile.js
@@ -1,9 +1,10 @@
 import express from "express"
 
-import { createProfile } from "../controller/profile"
+import { createProfile, getAll } from "../controller/profile"
 
 const router = express.Router()
 
 router.post("/profile", createProfile)
+router.get("/profile", getAll)
 
 export default router

0efbe637bbd4a1834a67016c9a4ad09ea70ab862
Thayse Onofrio <thayseonofrio@hotmail.com>
2020-07-12 19:11:51 -0300

    chore: refactor structure

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const databaseName = "afrolink"
-// todo - add production and dev connection
+
 mongoose
 	.connect(`mongodb://127.0.0.1:27017/${databaseName}`, {
 		useNewUrlParser: true,
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+	entry: "./client/src/index.js",
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/env"] }
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: "file-loader",
+					},
+				],
+			}
+		]
+	},
+	resolve: { extensions: ["*", ".js", ".jsx"] },
+	output: {
+		path: path.resolve(__dirname, "client/dist/"),
+		publicPath: "/client/dist/",
+		filename: "bundle.js"
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "client/public/"),
+		port: 3000,
+		publicPath: "http://localhost:3000/dist/",
+		hotOnly: true
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin()]
+}

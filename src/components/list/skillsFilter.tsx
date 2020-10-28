b6a2fa04cc077a76e289c325c68c27b8d22154a2
Thayse Onofrio <thayseonofrio@hotmail.com>
2020-10-28 19:07:55 -0300

    filter by skills (#8)

---
+++ src/components/list/skillsFilter.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Select,
+  MenuItem,
+  Checkbox,
+  ListItemText,
+  Input,
+  FormControl,
+  InputLabel,
+} from "@material-ui/core";
+import languages from "./../../../data/languages";
+
+type SkillsFilterProps = {
+  className: string;
+  onChange: any;
+  value: string[];
+};
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const skillsFilter = ({ className, onChange, value }: SkillsFilterProps) => {
+  return (
+      <Select
+        id="skills-filter"
+        className={className}
+        name="skills-filter"
+        label="Habilidades"
+        labelId="skills-label"
+        multiple
+        value={value}
+        onChange={onChange}
+        onBlur={onChange}
+        input={<Input />}
+        renderValue={(selected) => (selected as string[]).join(", ")}
+        MenuProps={MenuProps}
+      >
+        <MenuItem disabled key="default" value="Habilidades">
+            <ListItemText primary="Habilidades" />
+        </MenuItem>
+        {languages.map((skill) => (
+          <MenuItem key={skill} value={skill}>
+            <Checkbox checked={value.indexOf(skill) > -1} />
+            <ListItemText primary={skill} />
+          </MenuItem>
+        ))}
+      </Select>
+  );
+};
+
+export default skillsFilter;
